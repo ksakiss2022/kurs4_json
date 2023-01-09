84c7089569b46a2530a58d0b1d09afdd05a2d4c8
Swetlana <sweta_zaika@mail.ru>
2023-01-09 23:53:22 +0300

    курс 4 лекция 1- json

---
+++ src/main/java/com/example/kurs4/json/Kurs4JsonApplication.java
@@ -0,0 +1,13 @@
+package com.example.kurs4.json;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kurs4JsonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Kurs4JsonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kurs4/json/Kurs4JsonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kurs4.json;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kurs4JsonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
